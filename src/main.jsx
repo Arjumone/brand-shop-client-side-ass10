ae762cc5ff469049605cdac21b67a8339d6ebeb2
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-10-17 20:52:10 +0600

    added some components

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import AuthProvider from './provider/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,11 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <AuthProvider>
      <RouterProvider router={router} />
+     </AuthProvider>
   </React.StrictMode>,
 )
+
+// client repo:https://github.com/programming-hero-web-course-4/b8a10-brandshop-client-side-Arjumone
+
